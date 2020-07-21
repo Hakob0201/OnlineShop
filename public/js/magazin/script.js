7eb14e4fe70cab3810072da60e2368f6a4af4784
Hakob0201 <hakob.vardanyan.1996@gmail.com>
2020-07-21 23:06:56 +0400

    Uploud file

--- public/js/magazin/script.js
+++ public/js/magazin/script.js
@@ -145,6 +145,34 @@ $(".add-my-shop").click(function () {
                                 add
                             </div>
                         </div>
+                        <div class="fail_container">
+                            <div>
+                                <div id="one" style="
+">
+                                    <p>
+                                    Please upload documents only in 'pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png' &amp; 'text' format.
+                                    </p>
+
+                                    <div id="uploader_file">
+                                        <div class="uploadDoc">
+                                          <div class="">
+
+                                            <div class="fileUpload">
+                                              <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon">
+                                            </div>
+                                          </div>
+                                          <div>
+                                            <input type="text" class="form-control" name="" placeholder="Note">
+                                          </div>
+                                          <div class=""><a class="btn-check"><i class="fa fa-times"></i></a></div><!-- col-1 -->
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="btn btn-new"><i class="fa fa-plus"></i> Add new</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                     <section>
                         <input type="text" placeholder="Topic" id="topic">
@@ -162,6 +190,119 @@ $(".add-my-shop").click(function () {
     AddMyShop()
 })
 function AddMyShop() {
+
+
+    ///////////////////////////////////////////////////////////////
+
+
+
+
+
+    var button_fail = $('.fileUpload')
+    var uploader_fail = $('<input type="file" class="upload up" id="up">\n')
+    var fileTypes = ['pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png', 'txt'];  
+    button_fail.on('click', function () {
+        uploader_fail.click()
+    })
+
+    uploader_fail.on('change', function () {
+
+
+        readURL(uploader_fail[0])
+    })
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var extension = input.files[0].name.split('.').pop().toLowerCase(),  //file extension from input file
+                isSuccess = fileTypes.indexOf(extension) > -1;  //is extension in acceptable types
+
+            if (isSuccess) { //yes
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    if (extension == 'pdf'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/179/179483.svg');
+                    }
+                    else if (extension == 'docx'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/281/281760.svg');
+                    }
+                    else if (extension == 'rtf'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136539.svg');
+                    }
+                    else if (extension == 'png'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136523.svg');
+                    }
+                    else if (extension == 'jpg' || extension == 'jpeg'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136524.svg');
+                    }
+                    else if (extension == 'txt'){
+                        $('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136538.svg');
+                    }
+                    else {
+                        //console.log('here=>'+$(input).closest('.uploadDoc').length);
+                        $('.uploadDoc').find(".docErr").slideUp('slow');
+                    }
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+            else {
+                //console.log('here=>'+$(input).closest('.uploadDoc').find(".docErr").length);
+                $(input).closest('.uploadDoc').find(".docErr").fadeIn();
+                setTimeout(function() {
+                    $('.docErr').fadeOut('slow');
+                }, 9000);
+            }
+        }
+    }
+
+
+        $(document).on('change','.up', function(){
+            var id = $(this).attr('id'); /* gets the filepath and filename from the input */
+            var profilePicValue = $(this).val();
+            var fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
+            profilePicValue = profilePicValue.substr(fileNameStart + 1).substring(0,20); /* isolates the filename */
+            //var profilePicLabelText = $(".upl"); /* finds the label text */
+            if (profilePicValue != '') {
+                //console.log($(this).closest('.fileUpload').find('.upl').length);
+                $(this).closest('.fileUpload').find('.upl').html(profilePicValue); /* changes the label text */
+            }
+        });
+
+        $(document).on('click',".btn-new",function(){
+            alert()
+            $("#uploader_file").append('<div class="uploadDoc">\n' +
+                '                                          <div class="">\n' +
+                '\n' +
+                '                                            <div class="fileUpload">\n' +
+                '                                              <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon">\n' +
+                '\n' +
+                '                                              <input type="file" class="upload up" id="up" onchange="readURL(this);">\n' +
+                '                                            </div>\n' +
+                '                                          </div>\n' +
+                '                                          <div>\n' +
+                '                                            <input type="text" class="form-control" name="" placeholder="Note">\n' +
+                '                                          </div>\n' +
+                '                                          <div class=""><a class="btn-check"><i class="fa fa-times"></i></a></div><!-- col-1 -->\n' +
+                '                                        </div>');
+        });
+
+        $(document).on("click", "a.btn-check" , function() {
+            if($(".uploadDoc").length>1){
+                $(this).closest(".uploadDoc").remove();
+            }else{
+                alert("You have to upload at least one document.");
+            }
+        });
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////
+
     $(document).on('click','.modal-remove.a-m-s',function () {
     $(".modal.a-m-s").remove()
     })
